d185e83221edf19149e8f3eda678433bdc9c18d2
Jairo da Silva Junior <jrsilva@amazon.com>
2021-12-14 18:58:17 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example
+
+`pip3 install requirements`
+
+`python3 create_pv_from_volumes.py < pvcs.txt`
---
+++ create_pv_from_volumes.py
@@ -0,0 +1,36 @@
+import boto3
+import sys
+import jinja2
+
+client = boto3.client('ec2')
+
+for line in sys.stdin:
+    
+    print(line)
+    
+    pvc_namespace, pvc_name, pv = line.split(" ")
+
+    response = client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'tag:kubernetes.io/created-for/pv/name',
+                'Values': [
+                    pv.strip()
+                ]
+            }
+        ]
+    )
+    
+    if len(response['Volumes']) > 1:
+        exit()
+        
+    volume = response['Volumes'][0]
+    
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template("pv.yml.j2")
+    outputText = template.render(volume_id=volume['VolumeId'], az=volume['AvailabilityZone'], size=volume['Size'], pvc_name=pvc_name, pvc_namespace=pvc_namespace)
+    
+    f = open(f"{pv}.yaml", "w")
+    f.write(outputText)
+    f.close()
