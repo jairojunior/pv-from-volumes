589e1f5d6a0a9300f9d38c1469fdc6594dfc38c6
Jairo da Silva Junior <jrsilva@amazon.com>
2021-12-14 21:50:30 +0000

    Template fix and error handling.

--- create_pv_from_volumes.py
+++ create_pv_from_volumes.py
@@ -21,16 +21,16 @@ for line in sys.stdin:
         ]
     )
     
-    if len(response['Volumes']) > 1:
-        exit()
+    if len(response['Volumes']) != 1:
+        continue
         
     volume = response['Volumes'][0]
     
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template("pv.yml.j2")
-    outputText = template.render(volume_id=volume['VolumeId'], az=volume['AvailabilityZone'], size=volume['Size'], pvc_name=pvc_name, pvc_namespace=pvc_namespace)
+    outputText = template.render(volume_id=volume['VolumeId'], az=volume['AvailabilityZone'], size=volume['Size'], pvc_name=pvc_name, pvc_namespace=pvc_namespace, pv=pv)
     
-    f = open(f"{pv}.yaml", "w")
+    f = open(f"{pv.strip()}.yaml", "w")
     f.write(outputText)
     f.close()
